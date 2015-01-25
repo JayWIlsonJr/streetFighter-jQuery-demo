79ea9a25b587111c21ad49ab94f1681058592c79
Jay Wilson Jr <info@jaywilsonjr.com>
2015-01-24 21:44:37 -0500

    Added jQuery for inital mouse hover state

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  $('.ryu').on('mouseenter', function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .on('mouseleave', function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  });
+
+});
