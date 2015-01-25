11d2e84f59c87da47fc10669f07ecdfc8a794701
Jay Wilson Jr <info@jaywilsonjr.com>
2015-01-24 23:53:50 -0500

    Added hadouken audio, and hadouken throw event.

--- js/app.js
+++ js/app.js
@@ -7,6 +7,31 @@ $(document).ready(function() {
   .on('mouseleave', function() {
     $('.ryu-ready').hide();
     $('.ryu-still').show();
+  })
+  .on('mousedown', function() {
+    playHadouken();
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show()
+    .animate({'left': '300px'}, 500, 
+             function() {
+              $(this).hide();
+              $(this).css('left', '-212px');
+             });
+
+    //play hadouken sound
+    //throw hadouken
+  })
+  .on('mouseup', function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+    //ryu goes back to his ready position
   });
 
 });
+
+function playHadouken() {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
